58317a0ee6fecca031de55be745fb91a81ba03ef
Jorge E. Grisales <66390452+JorgeGrisales@users.noreply.github.com>
2020-11-19 11:16:14 -0500

    categoria.js completa

--- Backend/routes/categoria.js
+++ Backend/routes/categoria.js
@@ -3,14 +3,11 @@ const router = express.Router();
 const mysqlConnection =require('../db/db');
 
 const { Router } = require("express");
-const router = Router();
-
-const mysqlConnection = require("../db/db.js");
 
 // Categoria
 //Petición get
-router.get("/parque", (req, res) => {
-  mysqlConnection.query("SELECT * FROM parque", (err, rows, fields) => {
+router.get("/categoria", (req, res) => {
+  mysqlConnection.query("SELECT * FROM categoria", (err, rows, fields) => {
     if (!err) {
       res.status(200).json(rows);
     } else {
@@ -20,81 +17,51 @@ router.get("/parque", (req, res) => {
 });
 
 //Petición post
-router.post("/parque", (req, res) => {
+router.post("/categoria", (req, res) => {
   const {
     nombre,
-    comentario,
-    transporte,
-    recomendaciones,
-    historia,
-    direccion,
-    longitud,
-    latitud
+    referencia
   } = req.body;
 
-  let nuevoParque = `INSERT INTO parque (  nombre,
-    comentario,
-    transporte,
-    recomendaciones,
-    historia,
-    direccion,
-    longitud,
-    latitud ) VALUES (?,?,?,?,?,?,?,?,?)`;
+  let nuevaCategoria = `INSERT INTO categoria  (  nombre,
+    referencia ) VALUES (?,?,?)`;
 
   mysqlConnection.query(
-    nuevoParque,
+    nuevaCategoria,
     [
       nombre,
-      comentario,
-      transporte,
-      recomendaciones,
-      historia,
-      direccion,
-      longitud,
-      latitud
+      referencia
     ],
     (err, results, fields) => {
       if (err) {
         res.status(500);
       } else {
-        res.status(201).json({ message: `Parque ingresado` });
+        res.status(201).json({ message: `Categoria ingresada` });
       }
     }
   );
 });
 
 //Petición put
-router.put("/parque/:ID", (req, res) => {
+router.put("/categoria/:ID", (req, res) => {
   const {
     nombre,
-    comentario,
-    transporte,
-    recomendaciones,
-    historia,
-    direccion,
-    longitud,
-    latitud
+    referencia
   } = req.body;
   const { ID } = req.params;
 
-  let actualizarParque = `UPDATE parque SET nombre=?, comentario=?, transporte=?, recomendaciones=?, historia=?, direccion=?,  longitud=?, latitud = ? 
+  let actualizarCategoria = `UPDATE categoria SET nombre=?, referencia=?
   WHERE ID = ?`;
   mysqlConnection.query(
-    actualizarParque,
+    actualizarCategoria,
     [
       nombre,
-      comentario,
-      transporte,
-      recomendaciones,
-      historia,
-      direccion,
-      longitud,
-      latitud,
+      referencia,
       ID
     ],
     (err, rows, fields) => {
       if (!err) {
-        res.status(201).json({ status: `Parque actualizado con éxito` });
+        res.status(201).json({ status: `Categoria actualizada correctamente` });
       } else {
         res.status(500);
       }
@@ -103,14 +70,14 @@ router.put("/parque/:ID", (req, res) => {
 });
 
 //PETICIÓN O SERVICIO DELETE - ELIMINACIÓN DE DATOS
-router.delete("/parque/:ID", (req, res) => {
+router.delete("/categoria/:ID", (req, res) => {
   const { ID } = req.params;
   mysqlConnection.query(
-    `DELETE FROM parque WHERE ID =?`,
+    `DELETE FROM categoria WHERE ID =?`,
     [ID],
     (err, rows, fields) => {
       if ("!err") {
-        res.status(200).json({ status: `El parque ha sido eliminado` });
+        res.status(200).json({ status: `La categoria ha sido eliminada` });
       } else {
         res.status(500);
       }
