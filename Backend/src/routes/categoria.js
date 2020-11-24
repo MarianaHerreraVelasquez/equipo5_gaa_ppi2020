6b9b95b8ecbb6f7b0afa76bbf67f1f0dca60b6a1
Jorge E. Grisales <66390452+JorgeGrisales@users.noreply.github.com>
2020-11-24 14:09:09 -0500

    .

--- Backend/src/routes/categoria.js
+++ Backend/src/routes/categoria.js
@@ -1,3 +1,17 @@
 const express = require('express');//tabNIne
 const router = express.Router();
 const mysqlConnection =require('../db/db');
+
+const { Router } = require("express");
+
+// Categoria
+//Petición get
+router.get("/categoria", (req, res) => {
+  mysqlConnection.query("SELECT * FROM categoria", (err, rows, fields) => {
+    if (!err) {
+      res.status(200).json(rows);
+    } else {
+      res.status(500);
+    }
+  });
+});
