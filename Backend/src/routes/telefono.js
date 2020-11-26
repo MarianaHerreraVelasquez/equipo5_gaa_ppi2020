0526900b2166f8e0114031f9957ca93ed874eea8
Mariana Herrera Velásquez <66391232+MarianaHerreraVelasquez@users.noreply.github.com>
2020-11-26 18:06:28 -0500

    <

--- Backend/src/routes/telefono.js
+++ Backend/src/routes/telefono.js
@@ -56,3 +56,5 @@ router.post("/telefono", (req, res) => {
       let actualizarTelefono = `UPDATE telefono SET movil=?, fijo=?, prefijo=? 
       WHERE id = ?`;
       mysqlConnection.query(
+        actualizarTelefono,
+        [
